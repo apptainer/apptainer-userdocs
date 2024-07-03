ba385a1cfabf422316976a81fdbdc976624e2872
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2024-07-03 14:44:45 -0500

    update to 1.3.3
    Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

--- replacements.py
+++ replacements.py
@@ -12,7 +12,7 @@ def variableReplace(app, docname, source):
 # Add the needed variables to be replaced either on code or on text on the next
 # dictionary structure.
 variable_replacements = {
-    "{InstallationVersion}" : "1.3.2",
+    "{InstallationVersion}" : "1.3.3",
     "{admindocs}" : "https://apptainer.org/docs/admin/1.3",
     "{version}": "1.3",
     "{adminversion}": "1.3",
